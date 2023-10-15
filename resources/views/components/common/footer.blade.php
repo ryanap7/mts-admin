14cbabc05e7e9bdb9a178e7f619a906d380fd198
Ryan Aprianto <ryan.aprianto77777@gmail.com>
2023-10-15 14:42:12 +0700

    Done

--- resources/views/components/common/footer.blade.php
+++
@@ -1,3 +0,0 @@
-<p class="dark:text-white-dark text-center ltr:sm:text-left rtl:sm:text-right pt-6 absolute">©
-    <span id="footer-year">2020</span> Mandiri Technology Sejahtera, All Right Reserved
-</p>
